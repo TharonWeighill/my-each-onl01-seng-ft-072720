4450f1236850912e014f84dfd3476a6be01474ff
Tharon Weighill <tharonweighill@gmail.com>
2020-08-06 22:57:17 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,4 +1,10 @@
-collection = [1, 2, 3, 4]
-my_each(collection) do |i|
-  puts i
+list = ["ab", "cd", "ef"]
+
+def my_each(array)
+  i = 0
+  while i < array.length
+
+    yield(array[i])
+    i = i + 1
+  end
 end
