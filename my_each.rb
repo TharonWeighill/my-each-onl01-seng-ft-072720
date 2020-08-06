4049427f7e8080bf327d7a6fc64b2f6ee1cb74a4
Tharon Weighill <tharonweighill@gmail.com>
2020-08-06 22:53:56 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
-  # code here
+
+
+
+
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
 end
+
+hello(list) {|i| "Hello #{i} what's up?"}
